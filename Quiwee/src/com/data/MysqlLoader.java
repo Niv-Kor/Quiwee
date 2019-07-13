58af6834a8b6db622e05e4cb4996927f07e24cd9
Korach <Korach@Korach-PC>
2019-07-13 03:29:39 +0300

    Minor bug fixes

--- Quiwee/src/com/data/MysqlLoader.java
+++ Quiwee/src/com/data/MysqlLoader.java
@@ -29,24 +29,47 @@ public class MysqlLoader
 		return list;
 	}
 	
-	public static Object[][] getRows(Class<? extends Pullable> pullableClass) throws SQLException {
+	public static Object[][] getRows(Class<? extends Pullable> pullableClass) {
 		Pullable[] enumConstants = pullableClass.getEnumConstants();
 		return getRows(pullableClass, getOverallQuery(enumConstants[0].getTableName()));
 	}
 	
-	public static Object[][] getRows(Class<? extends Pullable> pullableClass, String query) throws SQLException {
+	public static Object[][] getRows(Class<? extends Pullable> pullableClass, String query) {
 		Pullable[] enumConstants = pullableClass.getEnumConstants();
 		MysqlColumn[] columns = new MysqlColumn[enumConstants.length];
 		
 		for (int i = 0; i < columns.length; i++)
 			columns[i] = enumConstants[i].getColumn();
 		
-		return MysqlModifier.getRows(query, columns);
+		try { return MysqlModifier.getRows(query, columns); }
+		catch (SQLException ex) { return new Object[0][enumConstants.length]; }
+	}
+	
+	public static Object[][] getKeywordRows(Class<? extends Pullable> pullableClass, String keyword, Pullable... fields) {
+		Pullable[] enumConstants = pullableClass.getEnumConstants();
+		String tableName = enumConstants[0].getTableName();
+		char firstChar = tableName.charAt(0);
+		String query = getOverallQuery(tableName);
+		
+		//build query
+		if (keyword != null && !keyword.equals("")) {
+			query = query.concat("AND (");
+			for (int i = 0; i < fields.length; i++) {
+				query = query.concat(firstChar + "." + fields[i].getColumn().getName() + " "
+								   + "LIKE '%" + keyword + "%'");
+				
+				if (i < fields.length - 1) query = query.concat(" OR ");
+				else query = query.concat(")");
+			}
+		}
+		
+		//retrieve corresponding rows
+		return getRows(pullableClass, query);
 	}
 	
 	private static String getOverallQuery(String tableName) {
 		return "SELECT * "
-			 + "FROM " + tableName + " "
+			 + "FROM " + tableName + " " + tableName.charAt(0) + " "
 			 + "WHERE user_id = '" + userKey + "'";
 	}
 }
