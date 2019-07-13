66b1f85d5d903dc28c4ea009d6c82895e3b6035a
Korach <Korach@Korach-PC>
2019-07-13 03:30:17 +0300

    Added advanced payment options.

--- Quiwee/src/com/GUI/windows/queue_dialog/QueueDialogWindow.java
+++ Quiwee/src/com/GUI/windows/queue_dialog/QueueDialogWindow.java
@@ -1,4 +1,4 @@
-package com.GUI.windows;
+package com.GUI.windows.queue_dialog;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
@@ -7,16 +7,14 @@ import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
-import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
-import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -24,49 +22,53 @@ import java.util.List;
 import java.util.concurrent.Callable;
 import javax.swing.DefaultListSelectionModel;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import com.GUI.constants.ColorConstants;
 import com.GUI.constants.FontConstants;
 import com.GUI.states.dashboard.calendar.CalendarGrid;
+import com.controllers.Controller;
 import com.controllers.QueuesController;
 import com.controllers.ServicesController;
 import com.data.objects.Client;
 import com.data.objects.Queue;
 import com.data.objects.Service;
-import com.main.User;
-import com.queue_management.ClientsRetriever;
+import com.finance.CashRegister;
 import javaNK.util.GUI.swing.components.FocusField;
 import javaNK.util.GUI.swing.components.InteractiveLabel;
-import javaNK.util.data.MysqlModifier;
 import javaNK.util.math.DimensionalHandler;
 
-public class QueueDialog extends JFrame
+public class QueueDialogWindow extends JFrame implements PropertyChangeListener
 {
 	private static final long serialVersionUID = 5348484787032161945L;
 	private static final String MESSAGE = "New queue appointment for ";
-	private static final Dimension DIM = new Dimension(620, 300);
+	private static final Dimension DIM = new Dimension(620, 500);
 	private static final DateTimeFormatter FORMAT = DateTimeFormatter.ofPattern("EEEE, dd/MM/yyyy HH:mm");
 	private static final String SEARCH_MESSAGE = "search name or phone number";
 	
 	private LocalDateTime startTime, endTime;
 	private CalendarGrid calendarGrid;
 	private JLabel clientName, timeLab;
-	private JPanel southPane;
-	private JTable clientTable;
+	private JPanel buttonsPane;
 	private Client selectedClient;
 	private Service selectedService;
+	private ClientsSelectorTable clientTable;
 	private JComboBox<String> servicesDropDown, endDropDown;
+	private JCheckBox useServicePriceBox;
 	private GridBagConstraints gridConstraints;
 	private QueuesController controller;
+	private CashRegister cashRegister;
 	private boolean existingQueue;
 	
-	public QueueDialog(CalendarGrid datedPanel, LocalDateTime date) {
+	public QueueDialogWindow(CalendarGrid datedPanel, LocalDateTime date) {
 		super(MESSAGE + date.format(FORMAT));
 		setSize(DIM);
 		setResizable(false);
@@ -74,27 +76,34 @@ public class QueueDialog extends JFrame
 		
 		this.controller = new QueuesController();
 		this.gridConstraints = new GridBagConstraints();
+		this.cashRegister = new CashRegister();
 		this.calendarGrid = datedPanel;
 		this.startTime = date;
 		this.endTime = date.plusMinutes(30);
 		
 		JPanel mainPanel = new JPanel(new BorderLayout());
-		mainPanel.setFocusable(true);
+		mainPanel.setFocusable(false);
 		mainPanel.requestFocus();
 		mainPanel.setPreferredSize(DIM);
 		mainPanel.setBackground(ColorConstants.BASE_COLOR);
 		
-		JPanel clientsPane = new JPanel(new BorderLayout());
-		clientsPane.setPreferredSize(DimensionalHandler.adjust(DIM, 50, 100));
+		JPanel clientsPane = new JPanel(new GridBagLayout());
+		clientsPane.setPreferredSize(DimensionalHandler.adjust(DIM, 50, 50));
 		clientsPane.setOpaque(false);
 		createClientPane(clientsPane);
 		mainPanel.add(clientsPane, BorderLayout.WEST);
 		
 		JPanel detailsPane = new JPanel(new BorderLayout());
-		detailsPane.setPreferredSize(DimensionalHandler.adjust(DIM, 50, 100));
+		detailsPane.setPreferredSize(DimensionalHandler.adjust(DIM, 50, 50));
 		detailsPane.setOpaque(false);
 		createDetailsPane(detailsPane);
 		mainPanel.add(detailsPane, BorderLayout.CENTER);
+		
+		JPanel southPane = new JPanel(new BorderLayout());
+		southPane.setPreferredSize(DimensionalHandler.adjust(DIM, 100, 50));
+		southPane.setOpaque(false);
+		createSouthPane(southPane);
+		mainPanel.add(southPane, BorderLayout.SOUTH);
 		
 		addWindowListener(new WindowListener() {
 			@Override
@@ -104,7 +113,7 @@ public class QueueDialog extends JFrame
 			public void windowClosed(WindowEvent e) {}
 
 			@Override
-			public void windowClosing(WindowEvent e) { calendarGrid.press(false); }
+			public void windowClosing(WindowEvent e) { calendarGrid.release(); }
 
 			@Override
 			public void windowDeactivated(WindowEvent e) {}
@@ -123,59 +132,43 @@ public class QueueDialog extends JFrame
 		pack();
 		setVisible(true);
 	}
-
+	
+	@Override
+	public void dispose() {
+		calendarGrid.release();
+		super.dispose();
+	}
+	
 	private void createClientPane(JPanel panel) {
 		//dimensions and panel creation
-		Dimension centerPaneDim = DimensionalHandler.adjust(panel, 100, 50);
-		Dimension tableDim = DimensionalHandler.adjust(centerPaneDim, 90, 100);
+		Dimension tableDim = DimensionalHandler.adjust(panel, 90, 80);
 		Dimension searchButtonDim = new Dimension(20, 20);
 		Dimension searchFieldDim = new Dimension(tableDim.width - searchButtonDim.width, searchButtonDim.height);
 		
-		//north search pane
-		JPanel northPane = new JPanel(new GridBagLayout());
-		northPane.setPreferredSize(DimensionalHandler.adjust(panel, 100, 25));
-		northPane.setOpaque(false);
-		
 		//table
-		this.clientTable = new JTable(0, 0);
+		this.clientTable = new ClientsSelectorTable();
 		clientTable.setPreferredSize(tableDim);
 		clientTable.setRowHeight(15);
 		clientTable.setBackground(Color.WHITE);
-		clientTable.addMouseListener(new MouseAdapter() {
-		    public void mousePressed(MouseEvent mouseEvent) {
-		        JTable table =(JTable) mouseEvent.getSource();
-		        Point point = mouseEvent.getPoint();
-		        int row = table.rowAtPoint(point);
-		        if (mouseEvent.getClickCount() == 2 && table.getSelectedRow() != -1) {
-		        	try {
-						String name = (String) table.getValueAt(row, 0);
-						String phone = (String) table.getValueAt(row, 1);
-						selectedClient = new Client(phone);
-						clientName.setText(name);
-						clientName.setForeground(ColorConstants.COLOR_2);
-					}
-					catch (SQLException ex) { ex.printStackTrace(); }
-		        }
-		    }
-		});
+		clientTable.subscribeToChanges(this);
 		
 		//search field and button
 		FocusField searchField = new FocusField(SEARCH_MESSAGE);
 		searchField.setPreferredSize(searchFieldDim);
 		searchField.setHorizontalAlignment(JTextField.CENTER);
-		addComponent(northPane, searchField, 0, 0);
+		gridConstraints.insets.top = 10;
+		addComponent(panel, searchField, 0, 0);
 		
 		JButton searchButton = new JButton("...");
 		searchButton.setPreferredSize(searchButtonDim);
 		searchButton.setFont(new Font("Arial", Font.PLAIN, 7));
-		searchButton.addActionListener(new ClientsRetriever(clientTable, searchField));
-		addComponent(northPane, searchButton, 1, 0);
-		panel.add(northPane, BorderLayout.NORTH);
-		
-		//center table pane
-		JPanel centerPane = new JPanel(new GridBagLayout());
-		centerPane.setPreferredSize(centerPaneDim);
-		centerPane.setOpaque(false);
+		searchButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				clientTable.search(searchField.getText());
+			}
+		});
+		addComponent(panel, searchButton, 1, 0);
 		
 		//force selection of only 1 row
 		DefaultListSelectionModel selectionModel = new DefaultListSelectionModel();
@@ -186,15 +179,8 @@ public class QueueDialog extends JFrame
 		//include table within a scroll pane
 		JScrollPane clientScrollPane = new JScrollPane(clientTable);
 		clientScrollPane.setPreferredSize(tableDim);
-		addComponent(centerPane, clientScrollPane, 0, 0);
-		panel.add(centerPane, BorderLayout.CENTER);
-		
-		//south select pane
-		JPanel southPane = new JPanel(new GridBagLayout());
-		southPane.setPreferredSize(DimensionalHandler.adjust(panel, 100, 25));
-		southPane.setOpaque(false);
-		
-		panel.add(southPane, BorderLayout.SOUTH);
+		gridConstraints.insets.right = -20;
+		addComponent(panel, clientScrollPane, 0, 1);
 	}
 	
 	private void createDetailsPane(JPanel panel) {
@@ -257,16 +243,19 @@ public class QueueDialog extends JFrame
 		addComponent(northEastPane, clientName, 1, 0);
 		
 		//services drop down menu
-		//retrieve all services from data base
-		String servicesQuery = "SELECT name FROM services WHERE user_id = '" + User.getKey() + "'";
-		List<String> servicesFromDB = new ArrayList<String>();
-		try { servicesFromDB = MysqlModifier.readAllVARCHAR(servicesQuery, "name"); }
-		catch (SQLException e) {}
+		//retrieve all services from database
+		Controller<Service> serviceCont = new ServicesController();
+		Object[][] servicesRows = serviceCont.getkeywordResults(null);
+		List<String> servicesNamesList = new ArrayList<String>();
+		
+		//add all services names to the list
+		for (int i = 0; i < servicesRows.length; i++)
+			servicesNamesList.add((String) servicesRows[i][1]);
 		
 		//create a new list with the options "-" and those that the data base has
 		List<String> serviceChoices = new ArrayList<String>();
 		serviceChoices.add("-");
-		serviceChoices.addAll(servicesFromDB);
+		serviceChoices.addAll(servicesNamesList);
 		
 		//convert the list to a String[] object
 		String[] serviceChoicesArr = serviceChoices.toArray(new String[serviceChoices.size()]);
@@ -276,11 +265,12 @@ public class QueueDialog extends JFrame
 		servicesDropDown.setFocusable(false);
 		servicesDropDown.setForeground(ColorConstants.TEXT_COLOR_DARK);
 		servicesDropDown.setFont(dropDownFont);
-		servicesDropDown.addPropertyChangeListener(new PropertyChangeListener() {
+		servicesDropDown.addItemListener(new ItemListener() {
 			@Override
-			public void propertyChange(PropertyChangeEvent e) {
-				ServicesController serviceController = new ServicesController();
-				selectedService = (Service) serviceController.getObj(servicesDropDown.getSelectedItem());
+			public void itemStateChanged(ItemEvent arg0) {
+				Controller<Service> serviceCont = new ServicesController();
+				selectedService = (Service) serviceCont.getObj(servicesDropDown.getSelectedItem());
+				if (selectedService != null) useServicePriceBox.setSelected(true);
 			}
 		});
 		
@@ -318,37 +308,123 @@ public class QueueDialog extends JFrame
 		
 		//update the end time in drop down menu and time range
 		updateEndTimeMenu();
+	}
+	
+	private void createSouthPane(JPanel panel) {
+		//advanced options pane 
+		JPanel advancedOptionsPane = new JPanel(new GridBagLayout());
+		advancedOptionsPane.setPreferredSize(DimensionalHandler.adjust(panel, 100, 60));
+		advancedOptionsPane.setOpaque(false);
+		panel.add(advancedOptionsPane, BorderLayout.CENTER);
 		
-		//south button panel
-		this.southPane = new JPanel(new GridBagLayout());
-		southPane.setPreferredSize(DimensionalHandler.adjust(panel, 100, 20));
-		southPane.setOpaque(false);
-		panel.add(southPane, BorderLayout.SOUTH);
+		//labels and fields
+		Font smallerFont = FontConstants.SMALL_LABEL_FONT.deriveFont((float) 12);
+		
+		JLabel priceLab = new JLabel("Price:");
+		priceLab.setFont(FontConstants.SMALL_LABEL_FONT);
+		gridConstraints.insets = new Insets(10, -452, 10, 10);
+		addComponent(advancedOptionsPane, priceLab, 0, 1);
+		
+		JTextField priceFld = new JTextField();
+		priceFld.setPreferredSize(new Dimension(80, 20));
+		priceFld.getDocument().addDocumentListener(new DocumentListener() {
+			@Override
+			public void removeUpdate(DocumentEvent e) { updateRegister(); }
+			
+			@Override
+			public void insertUpdate(DocumentEvent e) { updateRegister(); }
+			
+			@Override
+			public void changedUpdate(DocumentEvent e) {}
+			
+			private void updateRegister() {
+				try { cashRegister.setPrice(Double.parseDouble(priceFld.getText())); }
+				catch (NumberFormatException ex) { cashRegister.setPrice(-1); }
+			}
+		});
+		
+		gridConstraints.insets.left = -297;
+		addComponent(advancedOptionsPane, priceFld, 1, 1);
+		
+		JLabel currencyLab = new JLabel("NIS.");
+		currencyLab.setFont(smallerFont);
+		gridConstraints.insets.left = -177;
+		addComponent(advancedOptionsPane, currencyLab, 2, 1);
+		
+		JCheckBox hourlyPriceBox = new JCheckBox("Per hour");
+		hourlyPriceBox.setFont(smallerFont);
+		hourlyPriceBox.setOpaque(false);
+		hourlyPriceBox.setFocusable(false);
+		hourlyPriceBox.addPropertyChangeListener(new PropertyChangeListener() {
+			@Override
+			public void propertyChange(PropertyChangeEvent e) {
+				cashRegister.setAsPricePerHour(hourlyPriceBox.isSelected());
+			}
+		});
+		
+		gridConstraints.insets.top = -10;
+		gridConstraints.insets.left = -424;
+		addComponent(advancedOptionsPane, hourlyPriceBox, 0, 2);
+		
+		this.useServicePriceBox = new JCheckBox("Use service pricing");
+		useServicePriceBox.setFont(smallerFont);
+		useServicePriceBox.setOpaque(false);
+		useServicePriceBox.setFocusable(false);
+		useServicePriceBox.addPropertyChangeListener(new PropertyChangeListener() {
+			@Override
+			public void propertyChange(PropertyChangeEvent e) {
+				//apply service pricing
+				if (useServicePriceBox.isSelected())
+					cashRegister.priceByService(selectedService);
+				
+				//apply manual pricing as selected in the fields below
+				else {
+					try { cashRegister.setPrice(Double.parseDouble(priceFld.getText())); }
+					catch (NumberFormatException ex) { cashRegister.setPrice(0); }
+				
+					cashRegister.setAsPricePerHour(hourlyPriceBox.isSelected());
+				}
+			}
+		});
+		
+		gridConstraints.insets = new Insets(-30, -452, 10, 10);
+		addComponent(advancedOptionsPane, useServicePriceBox, 0, 0);
+		
+		//buttons panel
+		this.buttonsPane = new JPanel(new GridBagLayout());
+		buttonsPane.setPreferredSize(DimensionalHandler.adjust(panel, 100, 40));
+		buttonsPane.setOpaque(false);
+		panel.add(buttonsPane, BorderLayout.SOUTH);
 		
 		//queue creation button
-		InteractiveLabel createQueueBtn = new InteractiveLabel("Save");
-		createQueueBtn.setPreferredSize(new Dimension(90, 20));
-		createQueueBtn.setFont(FontConstants.SMALL_LABEL_FONT);
-		createQueueBtn.setForeground(ColorConstants.TEXT_COLOR_DARK);
-		createQueueBtn.enableSelectionColor(false);
-		createQueueBtn.setFunction(new Callable<Void>() {
+		InteractiveLabel saveBtn = new InteractiveLabel("Save");
+		saveBtn.setPreferredSize(new Dimension(90, 20));
+		saveBtn.setFont(FontConstants.SMALL_LABEL_FONT);
+		saveBtn.setForeground(ColorConstants.TEXT_COLOR_DARK);
+		saveBtn.enableSelectionColor(false);
+		saveBtn.setFunction(new Callable<Void>() {
 			@Override
 			public Void call() throws Exception {
 				if (selectedClient == null) return null;
 				
-				//remove previous queue first
-				if (existingQueue) calendarGrid.removeQueue();
-				
-				//add new one
-				controller.insertNew(selectedClient, selectedService, startTime, endTime);
-				
-				dispose();
+				try {
+					//remove previous queue first
+					if (existingQueue) calendarGrid.removeQueue();
+					
+					//add new one
+					controller.insertNew(selectedClient, selectedService, startTime, endTime);
+					
+					dispose();
+				}
+				catch (NullPointerException ex) { call(); }
 				return null;
 			}
 		});
 		
 		gridConstraints.insets = new Insets(10, 10, 10, 10);
-		addComponent(southPane, createQueueBtn, 1, 0);
+		addComponent(buttonsPane, saveBtn, 0, 0);
+		
+		clientTable.search(null);
 	}
 	
 	private void updateEndTimeMenu() {
@@ -379,6 +455,11 @@ public class QueueDialog extends JFrame
 		endDropDown.addItem("00:00");
 	}
 	
+	/**
+	 * Input a queue this dialog will show.
+	 * 
+	 * @param q - Queue to show
+	 */
 	public void inputQueueInfo(Queue q) {
 		existingQueue = true;
 		selectedClient = q.getClient();
@@ -386,16 +467,18 @@ public class QueueDialog extends JFrame
 		startTime = q.getStartTime();
 		updateEndTimeMenu();
 		endDropDown.setSelectedIndex(q.getDuration() / 30 - 1);
-		endTime = q.getStartTime();
-		endTime = endTime.plusMinutes(q.getDuration());
+		endTime = q.getStartTime().plusMinutes(q.getDuration());
 		timeLab.setText(getTimeRange(startTime, endTime));
 		
 		//change title
 		setTitle(getTitle().replaceAll("New", "Edit"));
 		
 		try { servicesDropDown.setSelectedItem(q.getService().getName()); }
-		//item might be not longer available
+		//item might no longer be available
 		catch (NullPointerException e) { servicesDropDown.setSelectedItem("-"); }
+		
+		//add extra buttons
+		gridConstraints.insets = new Insets(10, 10, 10, 10);
 		
 		//add delete button
 		InteractiveLabel deleteQueueBtn = new InteractiveLabel("Delete");
@@ -411,9 +494,30 @@ public class QueueDialog extends JFrame
 				return null;
 			}
 		});
+		addComponent(buttonsPane, deleteQueueBtn, 1, 0);
 		
-		gridConstraints.insets = new Insets(10, 10, 10, 10);
-		addComponent(southPane, deleteQueueBtn, 0, 0);
+		//add conclude button
+		InteractiveLabel concludeQueueBtn = new InteractiveLabel("Conclude");
+		concludeQueueBtn.setPreferredSize(new Dimension(90, 20));
+		concludeQueueBtn.setFont(FontConstants.SMALL_LABEL_FONT);
+		concludeQueueBtn.setForeground(ColorConstants.TEXT_COLOR_DARK);
+		concludeQueueBtn.enableSelectionColor(false);
+		JFrame currentFrame = this;
+		concludeQueueBtn.setFunction(new Callable<Void>() {
+			@Override
+			public Void call() throws Exception {
+				//check if checkout is successful
+				if (cashRegister.checkout(q)) dispose();
+				else {
+					JOptionPane.showMessageDialog(currentFrame,
+												  "Please enter a valid pricing.",
+												  "", JOptionPane.ERROR_MESSAGE, null);
+				}
+				
+				return null;
+			}
+		});
+		addComponent(buttonsPane, concludeQueueBtn, 2, 0);
 	}
 	
 	private String getTimeRange(LocalDateTime start, LocalDateTime end) {
@@ -437,7 +541,21 @@ public class QueueDialog extends JFrame
 	
 	private void addComponent(JPanel panel, Component component, int x, int y) {
 		gridConstraints.gridx = x;
-		gridConstraints.gridx = x;
+		gridConstraints.gridy = y;
 		panel.add(component, gridConstraints);
+	}
+
+	@Override
+	public void propertyChange(PropertyChangeEvent e) {
+		switch (e.getPropertyName()){
+			case "client selection": {
+				selectedClient = (Client) e.getNewValue();
+				
+				if (selectedClient != null) {
+					clientName.setText(selectedClient.getName());
+					clientName.setForeground(ColorConstants.COLOR_2);
+				}
+			}
+		}
 	}
 }
